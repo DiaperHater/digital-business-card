698b5b8662d41859ac097fd90264fd052ccbe6d8
Valery Pronin <pownmymail@gmail.com>
2023-01-09 23:11:47 +0300

    feat: About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,10 @@
 export default function About() {
     return(
-        <div className="">
-            <h2>About</h2>
+        <div className="about mt-5 px-[35px]">
+            <h3 className="about__title text-base font-bold mb-1">About</h3>
+            <p className="about__text text-xs">
+                I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </p>
         </div>
     )
 }
