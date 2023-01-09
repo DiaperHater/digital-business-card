3d1d1361ffdcfedd8c416825f50caddce78ae868
Valery Pronin <pownmymail@gmail.com>
2023-01-09 23:28:14 +0300

    feat: Interests component

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,7 +1,10 @@
 export default function Interests() {
     return(
-        <div className="">
-            <h2>Interests</h2>
+        <div className="interests mt-5 px-[35px]">
+            <h3 className="interests__title text-base font-bold mb-1">interests</h3>
+            <p className="interests__text text-xs">
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
         </div>
     )
 }
